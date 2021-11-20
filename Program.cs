79efb6f72c1c5324e0f84618613e452616c31f49
SavinaMaria <msavina00@mail.ru>
2021-11-20 11:20:49 +0300

    Revert "Конечный вид кода программы с комментариями"
    This reverts commit 3277f1e741edbc9e55865f6d642315d27350509e.

--- Program.cs
+++ Program.cs
@@ -2,23 +2,11 @@
 
 namespace ReferenceTypes
 {
-    //Класс "Программа"
     class Program
     {
         static void Main(string[] args)
         {
-            //Задание параметров классу 
-            Figure line = new Line(0, 1, 2, 3);
-            //Вызов метода 
-            line.Draw();
-            Figure circle = new Circle(5, 0, 0);
-            circle.Draw();
-            Figure rectangle = new Rectangle(1, 1, 10, 10);
-            rectangle.Draw();
-            Figure round = new Round(4, 4, 10, "purple");
-            round.Draw();
-            Figure ring = new Ring(4, 4, 10, 15, "purple");
-            ring.Draw();
+            
         }
     }
 }
