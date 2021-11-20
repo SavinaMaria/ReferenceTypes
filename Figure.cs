3277f1e741edbc9e55865f6d642315d27350509e
SavinaMaria <msavina00@mail.ru>
2021-11-20 10:41:53 +0300

    Конечный вид кода программы с комментариями

---
+++ Figure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReferenceTypes
+{
+    //Абстрактный класс Figure
+    public abstract class Figure
+    {
+        public Figure(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public int Y { get; set; }
+        public int X { get; set; }
+        //Объявление абстрактного метода Draw
+        public abstract void Draw();
+    }
+}
---
+++ Round.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReferenceTypes
+{
+    //Класс Round
+    public class Round : Figure
+    {
+        //Конструктор Round
+        public Round(int x, int y, int r, string colourfill) : base (x, y)
+        {
+            R = r;
+            ColourFill = colourfill;
+        }
+        public int R { get; set; }
+        public string ColourFill { get; set; }
+        //Использование метода Draw
+        public override void Draw()
+        {
+            Console.WriteLine("Круг. Координаты центра круга: х = {0}, у = {1}. Радиус равен {2}. Цвет заливки фигуры - {3}", X, Y, R, ColourFill);
+        }
+    }
+}
